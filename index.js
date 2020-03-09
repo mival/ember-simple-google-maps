a871d1e073a5474b39b6eea95d74bab2dc33abad
mival <jan.vonavka@gmail.com>
2020-03-09 15:26:53 +0100

    js load timeout catch

--- index.js
+++ index.js
@@ -11,8 +11,17 @@ module.exports = {
       if (config[this.name].apiKey !== '') {
         scriptUrl = scriptUrl + `&key=${config[this.name].apiKey}`;
       }
+      if (config[this.name].libraries && config[this.name].libraries.length) {
+        scriptUrl = scriptUrl + `&libraries=${config[this.name].libraries.join(',')}`;
+      }
     }
-    return scriptUrl !== '' ? `<script src="${scriptUrl}"></script>` : '';
+    return scriptUrl !== '' ? `<script>var s = document.createElement("script");
+    s.type = "text/javascript";
+    s.src = "${scriptUrl}";
+    s.async=true
+    s.defer=true
+    s.onerror="console.error"
+    $("body").append(s);</script>` : '';
   }
 
 };
